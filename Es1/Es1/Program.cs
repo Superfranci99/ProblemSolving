316a3fb75ab6f084d248e243e779b79a6c73ffac
Superfranci99 <superfranci99@gmail.com>
2015-02-27 13:32:59 +0100

    Es1 - lettura della richiesta

--- Es1/Es1/Program.cs
+++ Es1/Es1/Program.cs
@@ -11,6 +11,7 @@ namespace Es1
         static void Main(string[] args)
         {
             List<Regola> regole = LeggiRegole();
+            Richiesta richiesta = LeggiRichiesta();
         }
 
 
@@ -30,6 +31,21 @@ namespace Es1
             return result;
         }
 
+        static Richiesta LeggiRichiesta()
+        {
+            Console.Write("Inserisci il risultato... ");
+            char risultato = Console.ReadLine()[0];
+
+            Console.Write("Inserisci i parametri... ");
+            string[] parametriStr = Console.ReadLine().Replace(" ", "").Split(',');
+            List<char> parametri = new List<char>();
+            foreach (string s in parametriStr)
+                parametri.Add(s[0]);
+
+            return new Richiesta(parametri, risultato);
+        }
+
+
         class Regola
         {
             public int Sigla { get; set; }
@@ -48,6 +64,18 @@ namespace Es1
                 sep[1] = sep[1].Replace("[", "").Replace("]", "");
                 foreach (string ant in sep[1].Split(';'))
                     Antecedenti.Add(ant[0]);
+            }
+        }
+
+        class Richiesta
+        {
+            public List<char> Parametri { get; set; }
+            public char Risultato { get; set; }
+
+            public Richiesta(List<char> parametri, char risultato)
+            {
+                Parametri = parametri;
+                Risultato = risultato;
             }
         }
     }
