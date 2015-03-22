bf7747001100750461832d2d3a9a635f2ca3788b
Superfranci99 <superfranci99@gmail.com>
2015-03-22 14:44:37 +0100

    Es7 - creazione classe attivita

---
+++ Es7/Es7/Es7/Attivita.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es7
+{
+    class Attivita
+    {
+        public bool Iniziata   { get; private set; }
+        public bool Conclusa   { get; private set; }
+        public bool DaIniziare { get; private set; }
+
+        public List<Attivita> Precedenti { get; set; }
+        public List<Attivita> Successive { get; set; }
+
+        public int Durata { get; private set; }
+        public int Alunni { get; private set; }
+
+        public Attivita(int durata, int alunni)
+        {
+            this.Iniziata   = this.Conclusa = false;
+            this.DaIniziare = true;
+            this.Durata     = durata;
+            this.Alunni     = alunni;
+        }
+
+        public Attivita(int durata, int alunni, List<Attivita> precedenti, List<Attivita> successive)
+            : this(durata, alunni)
+        {
+            this.Precedenti = precedenti;
+            this.Successive = successive;
+        }
+    }
+}
