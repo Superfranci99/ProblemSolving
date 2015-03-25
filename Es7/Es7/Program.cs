b7a772b474a43fb1930bbd5d9ccbdedae1cf93d2
Superfranci99 <superfranci99@gmail.com>
2015-03-25 15:00:12 +0100

    Es7 - miglioramento output del programma

--- Es7/Es7/Program.cs
+++ Es7/Es7/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace Es7
 {
@@ -11,18 +10,27 @@ namespace Es7
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Francesco Bozzo - Esercizio 7\r\n");
+
+            // ottiene la prima e l'ultima attività
+            Console.Write("Inserisci la prima attività del progetto... ");
             int primaAttivita  = int.Parse(Console.ReadLine());
+            Console.Write("Inserisci l'ultima attività del progetto... ");
             int ultimaAttivita = int.Parse(Console.ReadLine());
-            GestoreAttivita gestore = new GestoreAttivita(listaAttivita, primaAttivita, ultimaAttivita);
-            gestore.EseguiProgetto();
-            Console.WriteLine("Il numero di giorni è {0}", gestore.Giorni);
-            Console.ReadLine();
-        }
 
             List<Attivita> listaAttivita = Parser.Parse("test.txt"); // legge i dati da file
 
+            // crea ed avvia la gestione delle attività
+            GestoreAttivita gestoreAttivita = new GestoreAttivita(listaAttivita, primaAttivita, ultimaAttivita);
+            gestoreAttivita.EseguiProgetto();
 
+            Console.WriteLine();
 
+            // mostra risultati
+            Console.WriteLine("Il numero di giorni è {0}", gestoreAttivita.Giorni);
+            Console.WriteLine("Il numero di alunni è {0}", GestoreAlunni.AlunniMinimi(listaAttivita, gestoreAttivita.Giorni));
+            Console.ReadLine();
         }
+
     }
 }
