8940a9dcbd61efd0679f77d45361d4dce16c02df
Superfranci99 <superfranci99@gmail.com>
2015-04-05 11:16:42 +0200

    Es7 - lettura di coppie di dipendenze
    Collegato a #2

--- Es7/Es7/Parser.cs
+++ Es7/Es7/Parser.cs
@@ -9,7 +9,7 @@ namespace Es7
 {
     static class Parser
     {
-        public static List<Attivita> Parse(string filename)
+        public static List<Attivita> Parse(string filename, bool coppiaDipendenze)
         {
             List<Attivita> listaAttivita = new List<Attivita>();
             StreamReader reader = new StreamReader(filename);
@@ -24,16 +24,32 @@ namespace Es7
                 listaAttivita.Add(new Attivita((i + 1).ToString(), durata, alunni));
             }
 
-            for (int i = 0; i < numAttivita; i++)
+            // se coppiaDipendenze è true, leggi le coppie di dipendenze come indicate nella consegna
+            // altrimenti leggi per ogni attività tutte le attività precedenti e successive tramite una lista per ciascuno
+            if (!coppiaDipendenze)
             {
-                string[] precedenti = reader.ReadLine().Replace(" ", "").Split(',');
-                string[] successive = reader.ReadLine().Replace(" ", "").Split(',');
-                if (precedenti[0] != "null")
-                    foreach (string s in precedenti)
-                        listaAttivita[i].Precedenti.Add(listaAttivita[int.Parse(s) - 1]);
-                if (successive[0] != "null")
-                    foreach (string s in successive)
-                        listaAttivita[i].Successive.Add(listaAttivita[int.Parse(s) - 1]);
+                for (int i = 0; i < numAttivita; i++)
+                {
+                    string[] precedenti = reader.ReadLine().Replace(" ", "").Split(',');
+                    string[] successive = reader.ReadLine().Replace(" ", "").Split(',');
+                    if (precedenti[0] != "null")
+                        foreach (string s in precedenti)
+                            listaAttivita[i].Precedenti.Add(listaAttivita[int.Parse(s) - 1]);
+                    if (successive[0] != "null")
+                        foreach (string s in successive)
+                            listaAttivita[i].Successive.Add(listaAttivita[int.Parse(s) - 1]);
+                }
+            }
+            else
+            {
+                while (!reader.EndOfStream)
+                {
+                    string[] dip = reader.ReadLine().Replace(" ", "").Split(',');
+                    int prec = int.Parse(dip[0]);
+                    int succ = int.Parse(dip[1]);
+                    listaAttivita[prec - 1].Successive.Add(listaAttivita[succ - 1]);
+                    listaAttivita[succ - 1].Precedenti.Add(listaAttivita[prec - 1]);
+                }
             }
 
             return listaAttivita;
