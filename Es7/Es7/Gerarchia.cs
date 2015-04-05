a04d7fb7740c1eb862f6e06f9ba0d3d13fe996d9
Superfranci99 <superfranci99@gmail.com>
2015-04-05 11:20:10 +0200

    Es7 - rilevamento prima e ultima attività
    This close #2

---
+++ Es7/Es7/Gerarchia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es7
+{
+    static class Gerarchia
+    {
+        public static Attivita PrimaAttivita(List<Attivita> listaAttivita)
+        {
+            Attivita primaAttivita = null;
+            listaAttivita.ForEach(x =>
+            {
+                if (x.Precedenti.Count == 0)
+                    primaAttivita = x;
+            });
+
+            return primaAttivita;
+        }
+
+        public static Attivita UltimaAttivita(List<Attivita> listaAttivita)
+        {
+            Attivita ultimaAttivita = null;
+            listaAttivita.ForEach(x =>
+            {
+                if (x.Successive.Count == 0)
+                    ultimaAttivita = x;
+            });
+
+            return ultimaAttivita;
+        }
+    }
+}
--- Es7/Es7/Program.cs
+++ Es7/Es7/Program.cs
@@ -12,19 +12,15 @@ namespace Es7
         {
             Console.WriteLine("Francesco Bozzo - Esercizio 7\r\n");
 
-            // ottiene la prima e l'ultima attività
-            Console.Write("Inserisci la prima attività del progetto... ");
-            int primaAttivita  = int.Parse(Console.ReadLine());
-            Console.Write("Inserisci l'ultima attività del progetto... ");
-            int ultimaAttivita = int.Parse(Console.ReadLine());
-
-            List<Attivita> listaAttivita = Parser.Parse("test.txt"); // legge i dati da file
-
+            // legge i dati da file
+            List<Attivita> listaAttivita = Parser.Parse("test8.txt", true); 
+            
             // crea ed avvia la gestione delle attività
-            GestoreAttivita gestoreAttivita = new GestoreAttivita(listaAttivita, primaAttivita, ultimaAttivita);
+            GestoreAttivita gestoreAttivita = new GestoreAttivita(
+                listaAttivita,
+                Gerarchia.PrimaAttivita(listaAttivita),
+                Gerarchia.UltimaAttivita(listaAttivita));
             gestoreAttivita.EseguiProgetto();
-
-            Console.WriteLine();
 
             // mostra risultati
             Console.WriteLine("Il numero di giorni è {0}", gestoreAttivita.Giorni);
